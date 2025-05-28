483051538df8f8a2a658ae68567e20d9a790fa4a
spidydevs <spidydevs@gmail.com>
2025-05-28 12:43:54 +0530

    Create comprehensive anime.js demo

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
 import anime from 'animejs';
 
-// Create grid of dots
-const dotsWrapper = document.querySelector('.dots');
-const gridSize = 25;
+// Create grid
+const grid = document.querySelector('.staggering-grid');
+const gridSize = 196; // 14x14 grid
 for (let i = 0; i < gridSize; i++) {
-  const dot = document.createElement('div');
-  dot.classList.add('dot');
-  dotsWrapper.appendChild(dot);
+  const gridItem = document.createElement('div');
+  gridItem.classList.add('grid-item');
+  const el = document.createElement('div');
+  el.classList.add('el');
+  gridItem.appendChild(el);
+  grid.appendChild(gridItem);
 }
 
-// Dots animation
+// Grid animation
 anime({
-  targets: '.dot',
+  targets: '.el',
   scale: [
-    { value: .1, duration: 500, easing: 'easeOutSine' },
-    { value: 1, duration: 1200, easing: 'easeInOutQuad' }
+    { value: .1, easing: 'easeOutSine', duration: 500 },
+    { value: 1, easing: 'easeInOutQuad', duration: 1200 }
   ],
-  delay: anime.stagger(200, { grid: [5, 5], from: 'center' }),
+  delay: anime.stagger(200, { grid: [14, 14], from: 'center' }),
   loop: true
 });
 
@@ -31,36 +34,52 @@ const shapesTimeline = anime.timeline({
 shapesTimeline
   .add({
     targets: '.square',
-    translateX: 270,
+    translateX: 100,
     rotate: 180,
-    scale: 2
+    scale: 1.5,
   })
   .add({
     targets: '.circle',
-    translateX: [{ value: 270, duration: 1000 }],
+    translateX: [{ value: -100 }],
     translateY: [
       { value: -50, duration: 500 },
       { value: 50, duration: 500 }
     ],
     rotate: 360
-  }, '-=1800')
+  }, '-=2000')
   .add({
     targets: '.triangle',
-    translateX: 270,
+    translateY: -100,
     rotate: 180,
     scale: 1.5
-  }, '-=1800');
+  }, '-=2000');
 
 // Path animation
-const pathEl = document.querySelector('.path');
-const pathLength = pathEl.getTotalLength();
-pathEl.setAttribute('stroke-dasharray', pathLength);
+const path = document.querySelector('.infinity-path');
+const pathLength = path.getTotalLength();
+path.setAttribute('stroke-dasharray', pathLength);
 
 anime({
-  targets: '.path',
+  targets: '.infinity-path',
   strokeDashoffset: [pathLength, 0],
   duration: 3000,
   loop: true,
-  easing: 'easeInOutSine',
-  direction: 'alternate'
+  direction: 'alternate',
+  easing: 'easeInOutSine'
+});
+
+// Text animation
+const text = document.querySelector('.letters');
+text.innerHTML = text.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime({
+  targets: '.letter',
+  opacity: [0, 1],
+  translateY: [-50, 0],
+  rotate: [-40, 0],
+  duration: 4000,
+  delay: anime.stagger(100),
+  loop: true,
+  direction: 'alternate',
+  easing: 'easeOutElastic(1, .6)'
 });
